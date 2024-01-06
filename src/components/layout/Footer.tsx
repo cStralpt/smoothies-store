ce850a1a30d3b34dad7f83fbc8931b7321c6b76c
Satria Alipatullah <satrialipatullah@gmail.com>
2024-01-06 18:46:20 +0700

    add footer navigation menu

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,56 @@
+import {
+  BadgeDollarSign,
+  CupSoda,
+  Home,
+  MapPinned,
+  UserRound,
+} from "lucide-react";
+
 export default function Footer() {
+  const iconSize = "h-6 w-6";
   return (
-    <footer className="footer sticky bottom-0 left-0 z-10 w-full border-4">
-      Footer
+    <footer className="footer sticky bottom-0 left-0 z-10 w-full border-t">
+      <nav className="w-full flex gap-4 justify-between text-sm text-gray-500">
+        {[
+          {
+            icon: <Home strokeWidth={1} className={`${iconSize}`} />,
+            label: "Home",
+          },
+          {
+            icon: <MapPinned strokeWidth={1} className={`${iconSize}`} />,
+            label: "Cabang",
+          },
+          {
+            icon: <CupSoda strokeWidth={1} className={`${iconSize}`} />,
+            label: "Menu",
+          },
+          {
+            icon: <BadgeDollarSign strokeWidth={1} className={`${iconSize}`} />,
+            label: "Transaksi",
+          },
+          {
+            icon: <UserRound strokeWidth={1} className={`${iconSize}`} />,
+            label: "Akun",
+          },
+        ].map((nav) => (
+          <button key={nav.label}>
+            <input
+              type="radio"
+              id={nav.label}
+              name="nav-menu"
+              className="hidden peer"
+              required
+            />
+            <label
+              htmlFor={nav.label}
+              className="hover:bg-gray-200/50 peer-checked:text-emerald-800 cursor-pointer px-4 py-2 rounded-xl flex flex-col items-center"
+            >
+              {nav.icon}
+              <span className="font-light">{nav.label}</span>
+            </label>
+          </button>
+        ))}
+      </nav>
     </footer>
   );
 }
