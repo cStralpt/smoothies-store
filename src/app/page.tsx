ab2a3dfc54774477f2b6e0bfc95878d811664ab9
Satria Alipatullah <satrialipatullah@gmail.com>
2024-01-06 21:24:11 +0700

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { getUserAuth } from "@/lib/auth/utils";
 export default async function Home() {
   const { session } = await getUserAuth();
   return (
-    <main className="space-y-4 grow">
+    <main className="grow">
       {session ? (
         <pre className="bg-card p-4 rounded-sm overflow-hidden">
           {JSON.stringify(session, null, 2)}
