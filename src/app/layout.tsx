a87b254f20ae41028ee9e561b2f4b6b3fe024f3a
Satria Alipatullah <satrialipatullah@gmail.com>
2024-01-06 20:14:22 +0700

    change bg to white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} h-screen overflow-hidden overflow-y-auto flex flex-col justify-between`}
+        className={`${inter.className} h-screen overflow-hidden overflow-y-auto flex flex-col justify-between bg-white`}
       >
         <ThemeProvider
           attribute="class"
