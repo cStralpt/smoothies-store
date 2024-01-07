d5548df2f581d8062e001d3e5240b4d99bcadd31
Satria Alipatullah <satrialipatullah@gmail.com>
2024-01-07 12:02:04 +0700

    changes design

--- src/app/locations/page.tsx
+++ src/app/locations/page.tsx
@@ -6,15 +6,17 @@ export default function Locations() {
   return (
     <main className="grow">
       <div className="p-4">
+        <div className="rounded-full overflow-hidden aspect-square border-4 mb-8">
+          <MapLocation
+            latitude={-8.6529017}
+            longitude={116.5350673}
+            height="350px"
+          />
+        </div>
         <b className="text-2xl text-emerald-950 flex gap-4 items-center">
           <Map className="w-16 h-16" />
           Lokasi Cabang King Smoothies
         </b>
-        <MapLocation
-          latitude={-8.6529017}
-          longitude={116.5350673}
-          height="350px"
-        />
       </div>
     </main>
   );
